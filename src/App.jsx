128348737f640f5699db93fb427926d6cb515d37
FatBoyLong <eugggggene@gmail.com>
2023-09-06 12:40:58 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+const StyledApp = styled.main`
+  border-color: orangered;
+  padding: 20px;
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button>Check in</Button>
+      </StyledApp>
+    </>
+  );
+}
+
+export default App;
